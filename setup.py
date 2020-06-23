c9b944fd32420e04573e069352b3b62185717eeb
Jack Higgins <1479627+skhg@users.noreply.github.com>
2020-06-23 16:17:38 +0200

    Add actions and remove travis (#5)
    * add actions and remove travis
    
    * update the setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='pytescomobileirl',
-    version='0.2.3',
-    description='Unofficial Python Tesco Mobile (Ireland) API',
-    long_description='See project page with usage examples at https://github.com/skhg/pytescomobileirl',
+    version='0.0.0',
+    description="Unofficial Python Tesco Mobile (Ireland) API",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='tesco mobile phone balance status web ireland api',
     author='Jack Higgins',
     author_email='pypi@jackhiggins.ie',
@@ -25,5 +31,4 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-    ])
+        'Programming Language :: Python :: 3.6'])
